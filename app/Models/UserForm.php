9b615e1614941ff3261c84aa7a980f15ef1cc310
andoattila698@gmail.com <andoattila698@gmail.com>
2023-12-11 18:58:07 +0100

    tr

--- app/Models/UserForm.php
+++ app/Models/UserForm.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
+
 
 class UserForm extends Model
 {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,13 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/logout', [HomeController::class, 'logout'])->name('logout');
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/logout', [HomeController::class, 'logout']);
+Route::get('/login', [AuthController::class, 'showLoginForm']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::get('/register', [RegisterController::class, 'showRegistrationForm']);
 Route::post('/register', [RegisterController::class, 'register']);
-Route::get('/update_profile', [ProfileController::class, 'showUpdateForm'])->name('update_profile');
+Route::get('/update_profile', [ProfileController::class, 'showUpdateForm']);
 Route::post('/update_profile', [ProfileController::class, 'updateProfile']);
 
 
